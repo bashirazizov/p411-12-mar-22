9493f73fe5e79b8561623173d2f83be98f1bc340
bashirazizov <bashir.azizov@code.edu.az>
2022-03-12 15:38:52 +0400

    init

---
+++ vesper/vesper/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using vesper.Areas.Admin.Models;
+using vesper.DAL;
+
+namespace vesper.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class DashboardController : Controller
+    {
+        private readonly VesperDbContext db;
+        public DashboardController(VesperDbContext _db)
+        {
+            db = _db;
+        }
+        public async Task<IActionResult> Index()
+        {
+            DashboardViewModel dvm = new DashboardViewModel();
+            dvm.TestimonialCount = await db.Testimonials.CountAsync();
+            dvm.PartnerCount = await db.Partners.CountAsync();
+            return View(dvm);
+        }
+    }
+}
